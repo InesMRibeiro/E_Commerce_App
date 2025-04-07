8f935573a175ec23497d37cb7d7337bbc87d28ef
Nelmah065 <127101802+Nelmah065@users.noreply.github.com>
2025-04-07 19:28:41 +0100

    Ajustes para tentar estabelecer conexão entre o backend Flask e a base de dados

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,39 @@
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import psycopg2
 
 app = Flask(__name__)
 CORS(app, resources={"/*": {"origins": "http://54.204.92.62"}}) # ADD IP WITHOUT PORT
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        host='13.48.24.89', 
+        database='ecommerce_db',
+        user='ecommerce_user',
+        password='password'
+    )
+    return conn
+
+#testando a conexão
+@app.route('/test_db', methods=['GET'])
+def test_db():
+    try:
+        conn = get_db_connection() # Conectar ao banco de dados
+        cur = conn.cursor()
+        cur.execute('SELECT 1;') # Realiza uma consulta simples
+        result = cur.fetchone() # Pega o resultado da consulta
+        cur.close()
+        conn.close()
+
+        if result:
+            return jsonify({"message": "Conexão bem-sucedida com o banco de dados!"}), 200
+        else:
+            return jsonify({"message": "Falha na conexão!"}), 500
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 # Lista de produtos (simulando um banco de dados)
 products = [
