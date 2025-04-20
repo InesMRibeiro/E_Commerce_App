96cbdcde5ccd432db8ba80c4c600867b5d4d30b3
InesMRibeiro <inesmribeiro4102@gmail.com>
2025-04-20 21:39:22 +0100

    tentativa de implementar load balancer

--- backend/config.py
+++ backend/config.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://inapp:inapp@44.208.20.119:5432/inapp_db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://inapp:inapp@16.171.60.198/inapp_db')
