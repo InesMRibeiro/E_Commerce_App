2df3d3c930bea9a3c9e5e78e41df78aeb37ce423
Nelmah065 <127101802+Nelmah065@users.noreply.github.com>
2025-04-22 00:56:39 +0100

    config.py fix

--- backend/config.py
+++ backend/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://inapp:password@51.20.52.166:5432/inapp_db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://inapp:inapp@51.20.52.166:5432/inapp_db')
     
