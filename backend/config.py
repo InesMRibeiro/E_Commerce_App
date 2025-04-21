bbd7f9bc43ca9f59051053ba2e6bff3da188bb3b
Nelmah065 <127101802+Nelmah065@users.noreply.github.com>
2025-04-22 00:54:52 +0100

    config database name changed

--- backend/config.py
+++ backend/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://inapp:password@51.20.52.166:5432/e_commerce_db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://inapp:password@51.20.52.166:5432/inapp_db')
     
