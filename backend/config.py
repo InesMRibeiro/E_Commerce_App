244bbf41df77172ee18b13e305f0fd9f649c1cfd
InesMRibeiro <inesmribeiro4102@gmail.com>
2025-04-25 23:19:09 +0100

    novo Ip DB

--- backend/config.py
+++ backend/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://inapp:inapp@51.21.248.169:5432/inapp_db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://inapp:inapp@13.61.153.137:5432/inapp_db')
     
