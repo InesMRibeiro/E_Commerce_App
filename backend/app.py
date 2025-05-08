89df2d720dcc420208c97270aadb3abb1f976700
InesMRibeiro <inesmribeiro4102@gmail.com>
2025-05-08 16:40:36 +0100

    novos ips

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 from functools import wraps
 
 app = Flask(__name__)
-CORS(app, resources={"/*": {"origins": "http://16.170.173.6"}})  # Frontend IP without port
+CORS(app, resources={"/*": {"origins": "http://51.20.181.188"}})  # Frontend IP without port
 app.config.from_object('config.Config')
 app.config['SECRET_KEY'] = 'insecure'  # VERY INSECURE: DO NOT USE IN PRODUCTION
 
--- backend/config.py
+++ backend/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:postgres@16.16.67.115:5432/inapp_db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:postgres@51.20.93.135:5432/inapp_db')
     
