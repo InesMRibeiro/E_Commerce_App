ca74421095252d2c05a8b4d76a6a6e5ed64f00db
InesMRibeiro <inesmribeiro4102@gmail.com>
2025-06-21 11:40:17 +0100

    novos ips aws

--- backend/config.py
+++ backend/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:postgres@13.61.175.146:5432/inapp_db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:postgres@16.171.2.5:5432/inapp_db')
     
